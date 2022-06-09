ecde2b4b6df89dc53c8458e113416dfc022685b3
davidcoats <david@getsafetycone.com>
2022-06-09 12:34:40 -0700

    Interim changes.

--- source/R5T.S0031/Code/Documentation.cs
+++ source/R5T.S0031/Code/Documentation.cs
@@ -4,7 +4,7 @@ using System;
 namespace R5T.S0031
 {
     /// <summary>
-    /// Script to Git commit and then push to GitHub all changes in all local repositories using a single commit message.
+    /// Pushkar functionality: A script to Git commit and then push to GitHub all changes in all local repositories using a single commit message.
     /// </summary>
     public static class Documentation
     {
